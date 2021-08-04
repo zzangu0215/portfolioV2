977cceec3e0ae18ac089c5521b81a312f545cc48
Junny <tajo0215@gmail.com>
2021-08-03 22:54:50 -0700

    projects route added

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const { User, Projects } = require("../models");
+const { Projects } = require("../models");
 
 // const withAuth = require("../util/withAuth");
 
@@ -8,6 +8,27 @@ router.get("/", async (req, res) => {
     res.render("home");
   } catch (err) {
     console.error(err);
+    res.status(500).send("⛔ Uh oh! An unexpected error occurred.");
+  }
+});
+
+router.get("/projects", async (req, res) => {
+  try {
+    const projectsData = await Projects.findAll({
+      where: {
+        user_id: 1,
+      },
+    });
+
+    const projects = projectsData.map((project) =>
+      project.get({ plain: true })
+    );
+
+    res.render("projects", {
+      projects,
+    });
+  } catch (err) {
+    console.log(err);
     res.status(500).send("⛔ Uh oh! An unexpected error occurred.");
   }
 });
